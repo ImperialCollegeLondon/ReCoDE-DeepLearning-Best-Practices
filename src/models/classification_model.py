9c45881933212168bd002493a22af5d808cbc24c
antonibigata <antonibigata@yahoo.es>
2024-01-22 19:20:54 +0000

    update readme

--- src/models/classification_model.py
+++ src/models/classification_model.py
@@ -65,11 +65,6 @@ class ClassificationModel(LightningModule):
     def forward(self, x: torch.Tensor):
         return self.net(x)
 
-    def on_train_start(self):
-        # by default lightning executes validation step sanity checks before training starts,
-        # so we need to make sure val_acc_best doesn't store accuracy from these checks
-        self.val_acc_best.reset()
-
     def step(self, batch: Any):
         x, y = batch
         logits = self.forward(x)
